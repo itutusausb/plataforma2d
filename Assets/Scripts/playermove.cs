34ecc829cc8c65086ef608e28e732c1dc0e07ff8
javentinuoc <byow@uoc.edu>
2021-05-09 13:39:05 +0200

    salto doble + caida

--- Assets/Scripts/playermove.cs
+++ Assets/Scripts/playermove.cs
@@ -7,7 +7,11 @@ public class playermove : MonoBehaviour
 {
     public float runSpeed = 2;
     public float jumpSpeed = 3;
+    public float doubleJumpSpeed = 2.5f;
+    private bool canDoubleJump;
+
     Rigidbody2D rigidbody2D;
+
     public bool betterJump = false;
     public float fallMultiplier = 0.5f;
     public float lowJumpMultiplier = 1f;
@@ -20,6 +24,56 @@ public class playermove : MonoBehaviour
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
+
+    private void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            if (CheckGround.isGrounded)
+            {
+                canDoubleJump = true;
+                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpSpeed);
+            }
+            else
+            {
+                if (Input.GetKeyDown("space"))
+                {
+                    if (canDoubleJump)
+                    {
+                        animator.SetBool("DoubleJump", true);
+                        rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, doubleJumpSpeed);
+                        canDoubleJump = false;
+                    }
+                }
+            }
+
+        }
+
+
+        if (CheckGround.isGrounded == false)
+        {
+            animator.SetBool("Jump", true);
+            animator.SetBool("Run", false);
+        }
+        if (CheckGround.isGrounded == true)
+        {
+            animator.SetBool("Jump", false);
+            animator.SetBool("DoubleJump", false);
+            animator.SetBool("Falling", false);
+        }
+
+        if (rigidbody2D.velocity.y < 0)
+        {
+            animator.SetBool("Falling", true);
+
+        }
+        else if (rigidbody2D.velocity.y > 0)
+
+            animator.SetBool("Falling", false);
+    }
+
+
+
 
     // Update is called once per frame
     void FixedUpdate()
@@ -34,7 +88,7 @@ public class playermove : MonoBehaviour
             rigidbody2D.velocity = new Vector2(runSpeed, rigidbody2D.velocity.y);
             spriteRenderer.flipX = false;
             animator.SetBool("Run", true);
-            
+
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
@@ -47,22 +101,10 @@ public class playermove : MonoBehaviour
             rigidbody2D.velocity = new Vector2(0, rigidbody2D.velocity.y);
             animator.SetBool("Run", false);
         }
-        if (Input.GetKey("space") && CheckGround.isGrounded)
-        {
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpSpeed);
-        }
-        if (CheckGround.isGrounded==false)
-        {
-            animator.SetBool("Jump", true);
-            animator.SetBool("Run", false);
-        }
-        if (CheckGround.isGrounded==true)
-        {
-            animator.SetBool("Jump", false);
-        }
+
         if (betterJump)
         {
-            if (rigidbody2D.velocity.y<0)
+            if (rigidbody2D.velocity.y < 0)
             {
                 rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
             }
@@ -73,6 +115,6 @@ public class playermove : MonoBehaviour
         }
     }
 
-    
 
 }
+
