313f6c751c042241a53193fc25e75de5a6e55bd6
javentinuoc <byow@uoc.edu>
2021-05-09 11:48:43 +0200

    cambio nivel

--- Assets/Scripts/FruitManager.cs
+++ Assets/Scripts/FruitManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FruitManager : MonoBehaviour
 {
@@ -13,6 +14,7 @@ public class FruitManager : MonoBehaviour
         if (transform.childCount == 0)
         {
             Debug.Log("No hay mas frutas");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
