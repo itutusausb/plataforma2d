d3ecc98eb3c00519a7a885501bddc37fd09afd4d
javentinuoc <byow@uoc.edu>
2021-05-11 23:00:22 +0200

    implantacion menu opciones

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject optionsPanel;
+
+    public void OptionsPanel()
+    {
+
+        Time.timeScale = 0;
+        optionsPanel.SetActive(true);
+    }
+
+    public void Return()
+    {
+        Time.timeScale = 1;
+        optionsPanel.SetActive(false);
+
+    }
+
+    public void AnotherOptions()
+    {
+        //Sonidos
+        //Graficos
+    }
+
+    public void GoMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
+
+
+
+}
