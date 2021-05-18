0e34f648f328ed193092b3d58cea27ec3e79038a
javentinuoc <byow@uoc.edu>
2021-05-18 19:58:04 +0200

    menu opciones y boton opciones

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    public GameObject optionsPanel;
+
+    public void OptionsPanel()
+    {
+        Time.timeScale = 0;
+        optionsPanel.SetActive(true);
+
+    }
+
+
+    public void Return()
+    {
+
+        Time.timeScale = 1;
+        optionsPanel.SetActive(false);
+    }
+
+    public void AnotherOptions()
+    {
+        //sound
+        //graphics
+
+    }
+
+    public void GoMainMenu()
+    {
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame() 
+    {
+        Application.Quit(); //para salir cuando generemos el ejecutable o la APK
+    }
+}
